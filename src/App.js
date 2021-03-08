ce24b583a99bc69b7e0e5c1c47b29f89c68b7921
Jake Linn <jake_linn@Jakes-MacBook-Air-2.local>
2021-03-07 20:14:40 -0500

    final

--- src/App.js
+++ src/App.js
@@ -1,5 +1,8 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
 import './App.css';
+import Characters from './components/Character';
+import axios from 'axios';
+
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
@@ -9,9 +12,40 @@ const App = () => {
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
 
+  const [starWars, setStarWars] = useState ([]);
+
+  useEffect(() => {
+axios 
+
+.get('https://swapi.dev/api/people/')
+
+.then(response => {
+  console.log(response);
+  setStarWars(response.data)
+})
+.catch(err => {
+  console.log('there is an error', err);
+})
+  }, [])
+
+ 
+  
+  
+
+
+  
+
+
+
+
   return (
+    <div>
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1>List of Star Wars Characters</h1>
+       {starWars.map((item) =>  
+<Characters starWars = {item} className= 'Header'/> 
+       )}
+    </div>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,34 @@
 // Write your Character component here
+import React from 'react'
+import Styled, {} from 'styled-components'
+
+
+
+
+const StyledCharacters = Styled.div `
+justify-conent: center;
+padding: 10px;
+color: white;
+border: green 5px solid;
+font-family: courier new;
+display: flex;
+font: 10px;
+
+
+
+`
+
+const Characters = (props) => {
+const {name, height} = props.starWars
+return (
+<StyledCharacters>
+<h2> Name: {name} </h2>
+<h2> Height: {height} CM </h2>
+
+</StyledCharacters>
+
+);
+
+};
+
+export default Characters;
